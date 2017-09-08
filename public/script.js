71099cb4c3598bb5b4f7eff5ef88c7ea4bc17eab
Noel Paganelli <noel@lacaspule.academy>
2017-09-08 12:27:59 +0200

    update UI and JS front end

---
+++ public/script.js
@@ -0,0 +1,27 @@
+$("document").ready(function() {
+
+  $("#count-contact").html($(".contact").length);
+
+  $('.avatar').click(function(){
+     $(this).closest("table").find(".data-contact").toggle(1000);
+  });
+
+  $('#add-form').submit(function(event){
+      if($('#add-firstname').val().length == 0 || $('#add-lastname').val().length == 0 || $('#add-email').val().length == 0 || $('#add-phone').val().length == 0) {
+        $("#add-error").html('<div class="alert alert-danger" role="alert">Vous devez renseigner tout les champs du formulaire </div>');
+        event.preventDefault();
+      } else if ($('#add-phone').val().length < 10) {
+        $("#add-error").html('<div class="alert alert-danger" role="alert">Vous devez mettre un numéro de téléphone valide</div>');
+        event.preventDefault();
+      }
+  });
+
+  $('.contact').find(".fa-pencil").click(function(){
+    console.log($(this).data("position"));
+    console.log($(this).data("property"));
+
+    $("#update-property").val($(this).data("property"));
+    $("#update-position").val($(this).data("position"));
+  });
+
+})
